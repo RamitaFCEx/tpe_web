fed6eaa37320bebe8d3e8204610d45f871edd687
Ramiro Meza <mezaramiroe@gmail.com>
2022-05-02 19:35:48 -0300

    retoques

--- js/app_captcha.js
+++ js/app_captcha.js
@@ -4,27 +4,27 @@ let clave = [];
 let textoClave;
 
 function generador(){
-for (i=0; i<5; i++){
-  console.log("-----");
-  let indice = Math.floor(Math.random()*11);
-  console.log(posiblesCaracteres[indice]);
+    for (i=0; i<5; i++){
+        console.log("-----");
+        let indice = Math.floor(Math.random()*11);
+        console.log(posiblesCaracteres[indice]);
 
-clave.push(posiblesCaracteres[indice])
-}
+        clave.push(posiblesCaracteres[indice])
+    }
 
-textoClave = clave.join("");
-console.log(textoClave);
+    textoClave = clave.join("");
+    console.log(textoClave);
 }
 
 function comprobarCaptcha(){
-    if(respuestaCaptcha.value == textoClave){//compara
-        resultado_captcha.innerHTML = "Validado";
+    if((respuestaCaptcha.value == textoClave)){//compara
+        resultado_captcha.innerHTML = "Captcha Validado";
     } else{
-        resultado_captcha.innerHTML = "Rechazado";
+        resultado_captcha.innerHTML = "Captcha Rechazado";
     }
-    return false;
     
 }
+
 
 generador();
 
@@ -38,3 +38,5 @@ let respuestaCaptcha = document.querySelector("#respuestaCaptcha");// respuesta 
 let boton_captcha = document.querySelector("#boton_captcha");
 
 boton_captcha.addEventListener("click", comprobarCaptcha);
+
+
