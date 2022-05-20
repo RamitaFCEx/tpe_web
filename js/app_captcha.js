d5190e11a41e1dc0287bdcdc2c8ea03fe3f9a802
Ramiro Meza <mezaramiroe@gmail.com>
2022-05-20 16:02:37 -0300

    detalle js

--- js/app_captcha.js
+++ js/app_captcha.js
@@ -6,15 +6,26 @@ const posiblesCaracteres = ['a', 'g', 'e', '4', 'r', 'p', '6', 'y', 'k','u','8']
 let clave = [];//ACA SE GUARDAN LOS CARACTERES DEL CAPTCHA POR SEPARADO
 let textoClave = "";//ACA SE GUARDA EL CAPTCHA EN FORMA DE STRING
 
-function generador(){//ACA SE CREA EL CAPTCHA
+let titulo = document.querySelector("#texto_captcha");//captcha que se muestra en pantalla
+
+let resultado_captcha = document.querySelector("#resultado_captcha");//h4 que dice si se valido o no
+
+let respuestaCaptcha = document.querySelector("#respuestaCaptcha");// respuesta del humano al captcha
+
+let boton_captcha = document.querySelector("#boton_captcha");
+
+boton_captcha.addEventListener("click", comprobarCaptcha);
+
+function generador(textoClave){//ACA SE CREA EL CAPTCHA
+    let textoClaveGenerador = textoClave;
     for (let i=0; i<5; i++){
         //console.log("-----");
         let indice = Math.floor(Math.random()*11);
         //console.log(posiblesCaracteres[indice]);
         clave.push(posiblesCaracteres[indice])
     }
-    textoClave = clave.join("");
-    console.log(textoClave);
+    textoClaveGenerador = clave.join("");
+    return textoClaveGenerador;
 }
 
 function comprobarCaptcha(e){
@@ -30,16 +41,8 @@ function comprobarCaptcha(e){
     }
 }
 
-generador();
-
-let titulo = document.querySelector("#texto_captcha");//captcha que se muestra en pantalla
+textoClave = generador(textoClave);
 titulo.innerHTML = textoClave;
 
-let resultado_captcha = document.querySelector("#resultado_captcha");//h4 que dice si se valido o no
 
-let respuestaCaptcha = document.querySelector("#respuestaCaptcha");// respuesta del humano al captcha
-
-let boton_captcha = document.querySelector("#boton_captcha");
-
-boton_captcha.addEventListener("click", comprobarCaptcha);
 
