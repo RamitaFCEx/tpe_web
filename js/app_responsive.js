20eac6ea7185b8c45d90e42b547e8ebc5d57b6dc
Ramiro Meza <mezaramiroe@gmail.com>
2022-05-29 12:38:36 -0300

    Responsive perfecto

--- js/app_responsive.js
+++ js/app_responsive.js
@@ -1,14 +1,14 @@
 "use strict";
 
 let ulMenuResponsive = document.querySelector(".menu_responsive");
-// let liMenuResponsive = document.querySelector(".ul_menu_responsive_li");
-// let aMenuResponsive = document.querySelector(".ul_menu_responsive_li_a");
+//let liMenuResponsive = document.querySelectorAll(".ul_menu_responsive_li");
+//let aMenuResponsive = document.querySelectorAll(".ul_menu_responsive_li_a");
 let conteinerMenuResponsive = document.querySelector(".container_menu_responsive");
 
  let botonMenuResponsive = document.querySelector(".button_toggle");
  botonMenuResponsive.addEventListener("click", function(){
      ulMenuResponsive.classList.toggle("responsive_display_ul");
      
-//     liMenuResponsive.classList.toggle("");
-//     aMenuResponsive.classList.toggle("");
+     //liMenuResponsive.classList.toggle("responsive_display_ul_li");
+    // aMenuResponsive.classList.toggle("responsive_display_ul_li_a");
 });
