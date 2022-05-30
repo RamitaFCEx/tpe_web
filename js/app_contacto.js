7ed5c66c17ea6b171f4b7a64ed973c8180f6730d
Ramiro Meza <mezaramiroe@gmail.com>
2022-05-29 22:43:37 -0300

    Cambio de estilo

---
+++ js/app_contacto.js
@@ -0,0 +1,20 @@
+let tabla_contacto = document.querySelector(".tabla_contacto");
+
+tabla_contacto.innerHTML =`
+<tr class="color-form1">
+<td>+54 9 011 457 147</td>
+<td>+598 9 011 457 148</td>
+</tr>
+<tr class="color-form2">
+<td>+54 9 011 457 149</td>
+<td>+598 9 011 457 150</td>
+</tr>
+<tr class="color-form1">
+<td>+54 9 011 457 151</td>
+<td>+598 9 011 457 152</td>
+</tr>
+<tr class="color-form2">
+<td>+54 9 011 457 153</td>
+<td>+598 9 011 457 154</td>
+</tr>
+`;
