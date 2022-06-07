a29cad0c3624455d6587e83e652be7b5d535c359
Ramiro Meza <mezaramiroe@gmail.com>
2022-06-07 03:04:25 -0300

    carga auto andando

--- js/app_captcha.js
+++ js/app_captcha.js
@@ -1,20 +1,13 @@
 /*-ESTE SITIO FUE DESARROLLADO POR DIAZ MANUEL(C2) Y MEZA RAMIRO(C11) GRUPO 10----------------------------*/
 "use strict";
-const posiblesCaracteres = ['a', 'g', 'e', '4', 'r', 'p', '6', 'y', 'k','u','8'];
 let textoClave = "";//ACA SE GUARDA EL CAPTCHA EN FORMA DE STRING
 
 let titulo = document.querySelector("#texto_captcha");//captcha que se muestra en pantalla
 
-let respuestaCaptcha = document.querySelector("#respuestaCaptcha");// respuesta del humano al captcha
+let usuario = [];
 
 let botonEnviar = document.querySelector("#boton_enviar");
 botonEnviar.addEventListener('click', imprimir);
-
-let formulario = document.querySelector("#formulario");
-
-let tablaDinamica = document.querySelector(".cuerpo_tablaD");
-
-let usuario = [];
 
 let botonEnviar3 = document.querySelector("#boton_enviar_3");
 botonEnviar3.addEventListener("click", function(e){
@@ -30,21 +23,18 @@ botonEnviar3.addEventListener("click", function(e){
 
 let botonReiniciar = document.querySelector("#boton_reiniciar");
 botonReiniciar.addEventListener("click", function(){
-    tablaDinamica.innerHTML = "";
-    usuarioPorDefecto1 = [];
-    usuarioPorDefecto2 = [];
-    usuarioPorDefecto3 = [];
+    document.querySelectorAll(".fila_dinamica").forEach(x => x.remove());
+    // tablaDinamica.innerHTML = "";
     usuario = [];
     //console.log("reinicio");
 });
 
 function generador(textoClave){//ACA SE CREA EL CAPTCHA
+    const posiblesCaracteres = ['a', 'g', 'e', '4', 'r', 'p', '6', 'y', 'k','u','8'];
     let clave = [];//ACA SE GUARDAN LOS CARACTERES DEL CAPTCHA POR SEPARADO
     let textoClaveGenerador = textoClave;
     for (let i=0; i<5; i++){
-        //console.log("-----");
         let indice = Math.floor(Math.random()*11);
-        //console.log(posiblesCaracteres[indice]);
         clave.push(posiblesCaracteres[indice])
     }
     textoClaveGenerador = clave.join("");
@@ -52,6 +42,7 @@ function generador(textoClave){//ACA SE CREA EL CAPTCHA
 }
 
 function comprobarCaptcha(){
+    let respuestaCaptcha = document.querySelector("#respuestaCaptcha");// respuesta del humano al captcha
     let resultado_captcha = document.querySelector("#resultado_captcha");//h4 que dice si se valido o no
     if(respuestaCaptcha.value == textoClave){//compara
         resultado_captcha.innerHTML = "Validado";
@@ -66,54 +57,41 @@ function comprobarCaptcha(){
     }
 }
 
-function verificarFavorito(diaria){
-    if(diaria.checked == true){
-
-        return "Si";
-    }else{
-        return "No";
-    }
-}
-
 function imprimir(e){
     e.preventDefault();
     comprobarCaptcha();
+    let formulario = document.querySelector("#formulario");
+    let tablaDinamica = document.querySelector(".cuerpo_tablaD");
     let comprobacion = comprobarCaptcha();
     let formData = new FormData(formulario);
     //Captura de datos:
     let nombre = formData.get("nombre");
     let correo = formData.get("correo");
     let diaria = document.querySelector("#diaria");
-    let favorito = verificarFavorito(diaria);
+    let favorito = "";
+
+    (diaria.checked == true) ? favorito = "Si" : favorito = "No";
     //   class="favoritos"
     let claseFavorito = ``;
     ((comprobacion === 1) && (favorito == "Si"))? claseFavorito = `favoritos` : claseFavorito = ``;
 
     if(comprobacion === 1){
-        // tablaDinamica.innerHTML += `
-        // <tr>
-        // <td class=${claseFavorito}>${nombre}</td>
-        // <td class=${claseFavorito}>${correo}</td>
-        // <td class=${claseFavorito}>True</td>
-        // <td class=${claseFavorito}>${favorito}</td>
-        // </tr>`;
         crearUsuario(usuario, nombre, correo, favorito);//agrega un objeto
 
         const fila = document.createElement("tr"); //dice que es una fila y pone nombre
         tablaDinamica.appendChild(fila);//crea la fila
+        fila.classList.add("fila_dinamica");
         let datosUsuario = [nombre, correo, "True", favorito]//reune los datos
 
         for(let j=0; j<4; j++){//imprime los datos 
-        const espacio = document.createElement("td");//dice que es una celda y pone nombre
-        let contenido = document.createTextNode(`${datosUsuario[j]}`);//crea el contenido
-        fila.appendChild(espacio);//crea la celda
-        espacio.appendChild(contenido);//escribe la celda
-        if(claseFavorito == `favoritos`){
-            espacio.classList.add(`${claseFavorito}`);//pone clase favorito
+            const espacio = document.createElement("td");//dice que es una celda y pone nombre
+            let contenido = document.createTextNode(`${datosUsuario[j]}`);//crea el contenido
+            fila.appendChild(espacio);//crea la celda
+            espacio.appendChild(contenido);//escribe la celda
+            if(claseFavorito == `favoritos`){
+                espacio.classList.add(`${claseFavorito}`);//pone clase favorito
+            }
         }
-        }
-        
-    
     }
 }
 
@@ -130,27 +108,69 @@ function crearUsuario(usuario, nombre, correo, favorito){
 }
 
 ///Datos precargados en tabla dinamica
-tablaDinamica.innerHTML = 
-`
-<tr class="color-form1">
-<td class="favoritos">Fidel Castro</td>
-<td class="favoritos">elFideee@gmail.com</td>
-<td class="favoritos">True</td>
-<td class="favoritos">Si</td>
-</tr>
-<tr>
-<td>Benedicto XVI</td>
-<td>beniGGWP@gmail.com</td>
-<td>True</td>
-<td>No</td>
-</tr>
-<tr>
-<td class="favoritos">Aristobulo Delvalle</td>
-<td class="favoritos">delvalle_ahri@gmail.com</td>
-<td class="favoritos">True</td>
-<td class="favoritos">Si</td>
-</tr>
-`;///Datos precargados en tabla dinamica
+function cargaPorDefectoTabla(){
+    const usuarios = [
+        {
+            0 : "Fidel Castro",
+            1 : "elFideee@gmail.com",
+            2 : "True",
+            3 : "True",
+        },
+        {
+            0 : "Benedicto XVI",
+            1 : "beniGGWP@gmail.com",
+            2 : "True",
+            3 : "False"
+        },
+        {
+            0 : "Aristobulo Delvalle",
+            1 : "delvalle_ahri@gmail.com",
+            2 : "True",
+            3 : "True"
+        }
+    ];
+
+    let tablaDinamica = document.querySelector(".cuerpo_tablaD");
+    
+
+        for(let h=0; h<3; h++){//cada usuario
+            const fila = document.createElement("tr"); //dice que es una fila y pone nombre
+            tablaDinamica.append(fila);//crea la fila
+            fila.classList.add("fila_dinamica");
+            
+            for(let j=0; j<4; j++){
+                const espacio = document.createElement('td');//dice que es una celda y pone nombre
+                console.log(usuarios[h][j]);
+                let contenido = document.createTextNode(`${usuarios[h][j]}`);//crea el contenido
+                fila.appendChild(espacio);//crea la celda
+                espacio.appendChild(contenido);//escribe la celda
+                
+            }
+        }
+           
+    
+
+    // tablaDinamica.innerHTML = 
+    // `
+    // <tr class="color-form1 fila_dinamica">
+    // <td class="favoritos">True</td>
+    // <td class="favoritos">Si</td>
+    // </tr>
+
+    // <tr class="fila_dinamica">
+    // <td>True</td>
+    // <td>No</td>
+    // </tr>
+
+    // <tr class="fila_dinamica">
+    // <td class="favoritos">True</td>
+    // <td class="favoritos">Si</td>
+    // </tr>
+    // `;///Datos precargados en tabla dinamica
+}
+
+cargaPorDefectoTabla();
+    
 
 textoClave = generador(textoClave);
 titulo.innerHTML = textoClave;
