927eb4fe8c030a6d69bc0e150417ad2bbe340e86
Ramiro Meza <mezaramiroe@gmail.com>
2022-06-25 18:21:38 -0300

    responsive

--- js/responsive.js
+++ js/responsive.js
@@ -1,12 +1,15 @@
 /*-ESTE SITIO FUE DESARROLLADO POR DIAZ MANUEL(C2) Y MEZA RAMIRO(C11) GRUPO 10----------------------------*/
 "use strict";
 document.addEventListener('DOMContentLoaded', function menuResponsive(){
-    let ulMenuResponsive = document.querySelector(".menu_responsive");
+    let menuResponsive = document.querySelector(".menu_desktop");
      // let conteinerMenuResponsive = document.querySelector(".container_menu_responsive");
     
      let botonMenuResponsive = document.querySelector(".button_toggle");
      botonMenuResponsive.addEventListener("click", function(){
-         ulMenuResponsive.classList.toggle("responsive_display_ul");
+        //  botonMenuResponsive.classList.toggle("prueba");
+         menuResponsive.classList.toggle("responsive_display");
+        // menuResponsive.classList.toggle("responsive_display");
+
     });// hasta aca responsive
 
 
@@ -19,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function menuResponsive(){
         botonHome.classList.toggle("validandoCaptcha");
         indexArticle.innerHTML = "<h2>Loading</h2>";
         try{
-            let response = await fetch("http://127.0.0.1:5500/index_article.html");
+            let response = await fetch("http://192.168.0.154:5500/index_article.html");
             if (response.ok) {
             let t = await response.text()
             indexArticle.innerHTML = t;
@@ -33,11 +36,10 @@ document.addEventListener('DOMContentLoaded', function menuResponsive(){
     })
 
     let botonContacto = document.querySelector("#boton_contacto");
-    botonContacto.addEventListener("click", async function(e){
+    botonContacto.addEventListener('click', async function(e){
         e.preventDefault();
-        botonContacto.classList.toggle("validandoCaptcha");
         try{
-            let response = await fetch("http://127.0.0.1:5500/contacto_article.html");
+            let response = await fetch("http://192.168.0.154:5500/contacto_article.html");
             if (response.ok) {
                 let t = await response.text()
                 indexArticle.innerHTML = t;
@@ -57,9 +59,36 @@ document.addEventListener('DOMContentLoaded', function menuResponsive(){
         catch(error){
             indexArticle.innerHTML = "<h1>Connection error</h1>";
         }
-        
-
     });
+    
+    
+        // botonContacto.addEventListener("click", async function(e){
+        //     e.preventDefault();
+        //     try{
+        //         let response = await fetch("http://localhost:5500/contacto_article.html");
+        //         if (response.ok) {
+        //             let t = await response.text()
+        //             indexArticle.innerHTML = t;
+        //             if(document.querySelector("#captchaRef")){
+        //                 document.querySelector("#captchaRef").remove();
+        //             }
+        //             if(document.querySelector("#contactoJs")){
+        //                 document.querySelector("#contactoJs").remove();
+        //             }
+        //             let fileref = document.createElement('script');         
+        //             fileref.setAttribute('type', 'text/javascript');         
+        //             fileref.setAttribute('src', 'js/app_contacto.js');
+        //             fileref.setAttribute('id', 'contactoJs')     
+        //             document.getElementsByTagName('head')[0].appendChild(fileref);
+        //         }
+        //     }
+        //     catch(error){
+        //         indexArticle.innerHTML = "<h1>Connection error</h1>";
+        //     }
+        // });
+    
+
+    
 
 
 
@@ -71,7 +100,7 @@ document.addEventListener('DOMContentLoaded', function menuResponsive(){
         e.preventDefault();
         botonRegistro.classList.toggle("validandoCaptcha");
         try{
-            let response = await fetch("http://127.0.0.1:5500/registro_article.html");
+            let response = await fetch("http://192.168.0.154:5500/registro_article.html");
             if (response.ok) {
                 let t = await response.text()
                 indexArticle.innerHTML = t;
