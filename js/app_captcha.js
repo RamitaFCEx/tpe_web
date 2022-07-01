7df7b9a164bf6870d6d0018eafc3da488827272f
Ramiro Meza <mezaramiroe@gmail.com>
2022-06-30 22:19:01 -0300

    btn reinicio ok, errores de mockapi.io

--- js/app_captcha.js
+++ js/app_captcha.js
@@ -3,7 +3,6 @@
 
 var botonEnviar = document.querySelector("#boton_enviar");
 botonEnviar.addEventListener('click', imprimir);
-var usuariosRegistrados = [];
 
 var botonEnviar3 = document.querySelector("#boton_enviar_3");
 botonEnviar3.addEventListener("click", function(e){
@@ -16,47 +15,37 @@ botonEnviar3.addEventListener("click", function(e){
 });
 
 var botonReiniciar = document.querySelector("#boton_reiniciar");
-botonReiniciar.addEventListener("click", function(){
+botonReiniciar.addEventListener("click", async function(){
     document.querySelectorAll(".fila_dinamica").forEach(x => x.remove());
-    usuariosRegistrados = [];
-    console.log(usuariosRegistrados);
+    let arregloIdes = [];
+    try{
+        let listaUsuarios = await fetch(`https://62b8b677f4cb8d63df61b878.mockapi.io/api/R-OS/usuarios`);
+        if (listaUsuarios.ok) {
+            let objetoUsuarios = await listaUsuarios.json();
+            for(let individuo of (objetoUsuarios)){
+                arregloIdes.push(individuo.id);
+            }
+            console.log(arregloIdes);
+        }
+    }catch(error){
+            console.log("Falla de Base de datos eliminada");
+    }
+    try{
+        let r=0;
+        while(r<arregloIdes.length){
+            let response = await fetch(`https://62b8b677f4cb8d63df61b878.mockapi.io/api/R-OS/usuarios/${arregloIdes[r]}`, {
+            "method" : "DELETE"
+            });
+            if (response.ok) {
+                console.log("Base de datos eliminada");
+            }
+            r++
+        }
+        
+    }catch(error){
+        console.log("Falla de Base de datos eliminada");
+    }   
 });//trae todas las filas, las borra una por una
-
-
-
-function generador(){//ACA SE CREA EL CAPTCHA
-    const posiblesCaracteres = ['a', 'g', 'e', '4', 'r', 'p', '6', 'y', 'k','u','8'];
-    let clave = [];//ACA SE GUARDAN LOS CARACTERES DEL CAPTCHA POR SEPARADO
-    let textoClaveGenerador = "";
-    let titulo = document.querySelector("#texto_captcha");//captcha que se muestra en pantalla
-    for (let i=0; i<5; i++){
-        let indice = Math.floor(Math.random()*posiblesCaracteres.length);
-        clave.push(posiblesCaracteres[indice])
-    }
-    textoClaveGenerador = clave.join("");
-    titulo.innerHTML = textoClaveGenerador;
-
-    ///QUITAR
-    let respuestaCodeada = document.querySelector("#respuestaCaptcha");
-    respuestaCodeada.value = textoClaveGenerador;
-}
-
-function comprobarCaptcha(){
-    let respuestaCaptcha = document.querySelector("#respuestaCaptcha");// respuesta del humano al captcha
-    let resultado_captcha = document.querySelector("#resultado_captcha");//h4 que dice si se valido o no
-    let captcha = document.querySelector("#texto_captcha");//Captcha generado
-    if((respuestaCaptcha.value == captcha.innerHTML)){//compara
-        resultado_captcha.innerHTML = "Validado";
-        resultado_captcha.classList.add("validandoCaptcha");
-        resultado_captcha.classList.remove("rechazandoCaptcha");
-        return 1;
-    } else{
-        resultado_captcha.innerHTML = "Rechazado";
-        resultado_captcha.classList.add("rechazandoCaptcha");
-        resultado_captcha.classList.remove("validandoCaptcha");
-        return -1;
-    }
-}
 
 function imprimir(e){
     e.preventDefault();
@@ -71,9 +60,6 @@ function imprimir(e){
     let favorito = "";//Atributo del usuario
     
     (diaria.checked == true) ? favorito = "true" : favorito = "false";
-
-    let claseFavorito = ``;//Condicion para resaltar fila
-    ((comprobacion === 1) && (favorito == "true"))? claseFavorito = `favoritos` : claseFavorito = ``;
 
     if(comprobacion === 1){
         //crearUsuario(usuariosRegistrados, nombre, correo, favorito);//agrega un objeto
@@ -101,6 +87,56 @@ async function agregarUsuario(datosUsuarioNuevo){
         console.log("Falla de edicion");
     }
 }
+
+///Datos precargados en tabla dinamica   
+async function cargaTabla(){
+    try{
+        let tablaDinamica = document.querySelector(".cuerpo_tablaD");
+        let response = await fetch("https://62b8b677f4cb8d63df61b878.mockapi.io/api/R-OS/usuarios");
+        if (response.ok) {
+            let objetoUsuarios = await response.json();
+            tablaDinamica.innerHTML = "";
+            for(let individuo of (objetoUsuarios)){
+                const fila = document.createElement("tr"); //dice que es una fila y pone nombre
+                fila.setAttribute('id', `${individuo.id}`); 
+                tablaDinamica.append(fila);//crea la fila
+                fila.classList.add("fila_dinamica");//esta clase sirve para borrarla tabla
+        
+                for(let j in individuo){
+                    const espacio = document.createElement('td');//dice que es una celda y pone nombre
+                    let contenido = document.createTextNode(`${individuo[j]}`);//crea el contenido
+                    if(j != "id"){
+                        fila.appendChild(espacio);//crea la celda
+                        espacio.appendChild(contenido);//escribe la celda
+                        if(individuo.fav == true||individuo.fav == "true"){
+                            espacio.classList.add(`favoritos`);//pinta las celdas favoritas
+                        }
+                    }
+                    
+                }
+                for(let j=0; j<2; j++){//imprime los botones
+                    const espacio = document.createElement("td");//dice que es una celda y pone nombre
+                    let contenido =  document.createElement("button");//Crea botones
+                    contenido.innerHTML = (j==0)? "Editar":"Borrar";
+                    contenido.classList.add(`${contenido.innerHTML}`);
+                    
+                    
+                    fila.appendChild(espacio);//crea la celda
+                    espacio.appendChild(contenido);//escribe la celda
+                    if(espacio.previousSibling.classList[0] === `favoritos`){
+                        espacio.classList.add(`favoritos`);//pone clase favorito
+                    }
+                }
+            }
+            asignarEventoBorrar();
+            asignarEventoEditar();
+        }
+    }
+    catch(error){
+        console.log("<h1>Connection error</h1>");
+    }
+}
+
 
 function asignarEventoBorrar(){
     let botonBorrar = document.querySelectorAll(".Borrar");
@@ -152,53 +188,37 @@ function asignarEventoEditar(){
     }
 }
 
-
-///Datos precargados en tabla dinamica   
-async function cargaTabla(){
-    try{
-        let tablaDinamica = document.querySelector(".cuerpo_tablaD");
-        tablaDinamica.innerHTML = "";
-        let response = await fetch("https://62b8b677f4cb8d63df61b878.mockapi.io/api/R-OS/usuarios");
-        if (response.ok) {
-            let objetoUsuarios = await response.json();
-            for(let individuo of (objetoUsuarios)){
-                const fila = document.createElement("tr"); //dice que es una fila y pone nombre
-                fila.setAttribute('id', `${individuo.id}`); 
-                tablaDinamica.append(fila);//crea la fila
-                fila.classList.add("fila_dinamica");//esta clase sirve para borrarla tabla
-        
-                for(let j in individuo){
-                    const espacio = document.createElement('td');//dice que es una celda y pone nombre
-                    let contenido = document.createTextNode(`${individuo[j]}`);//crea el contenido
-                    if(j != "id"){
-                        fila.appendChild(espacio);//crea la celda
-                        espacio.appendChild(contenido);//escribe la celda
-                        if(individuo.fav == true||individuo.fav == "true"){
-                            espacio.classList.add(`favoritos`);//pinta las celdas favoritas
-                        }
-                    }
-                    
-                }
-                for(let j=0; j<2; j++){//imprime los botones
-                    const espacio = document.createElement("td");//dice que es una celda y pone nombre
-                    let contenido =  document.createElement("button");//Crea botones
-                    contenido.innerHTML = (j==0)? "Editar":"Borrar";
-                    contenido.classList.add(`${contenido.innerHTML}`);
-                    
-                    
-                    fila.appendChild(espacio);//crea la celda
-                    espacio.appendChild(contenido);//escribe la celda
-                    if(espacio.previousSibling.classList[0] === `favoritos`){
-                        espacio.classList.add(`favoritos`);//pone clase favorito
-                    }
-                }
-            }
-            asignarEventoBorrar();
-            asignarEventoEditar();
-        }
+function generador(){//ACA SE CREA EL CAPTCHA
+    const posiblesCaracteres = ['a', 'g', 'e', '4', 'r', 'p', '6', 'y', 'k','u','8'];
+    let clave = [];//ACA SE GUARDAN LOS CARACTERES DEL CAPTCHA POR SEPARADO
+    let textoClaveGenerador = "";
+    let titulo = document.querySelector("#texto_captcha");//captcha que se muestra en pantalla
+    for (let i=0; i<5; i++){
+        let indice = Math.floor(Math.random()*posiblesCaracteres.length);
+        clave.push(posiblesCaracteres[indice])
     }
-    catch(error){
-        console.log("<h1>Connection error</h1>");
+    textoClaveGenerador = clave.join("");
+    titulo.innerHTML = textoClaveGenerador;
+
+    ///QUITAR
+    let respuestaCodeada = document.querySelector("#respuestaCaptcha");
+    respuestaCodeada.value = textoClaveGenerador;
+}
+
+function comprobarCaptcha(){
+    let respuestaCaptcha = document.querySelector("#respuestaCaptcha");// respuesta del humano al captcha
+    let resultado_captcha = document.querySelector("#resultado_captcha");//h4 que dice si se valido o no
+    let captcha = document.querySelector("#texto_captcha");//Captcha generado
+    if((respuestaCaptcha.value == captcha.innerHTML)){//compara
+        resultado_captcha.innerHTML = "Validado";
+        resultado_captcha.classList.add("validandoCaptcha");
+        resultado_captcha.classList.remove("rechazandoCaptcha");
+        return 1;
+    } else{
+        resultado_captcha.innerHTML = "Rechazado";
+        resultado_captcha.classList.add("rechazandoCaptcha");
+        resultado_captcha.classList.remove("validandoCaptcha");
+        return -1;
     }
 }
 
